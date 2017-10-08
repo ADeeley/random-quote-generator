b9d6a4d83e7a93a0b3d44a4329ee546a3d066da8
Adam Deeley <ad.deeley@gmail.com>
2017-10-08 12:19:51 +0100

    Working quote-me button.

---
+++ main.js
@@ -0,0 +1,21 @@
+quotes = {
+    "Anonymous" : "\"There is no way you will get a quote from me for your stupid website.\"",
+    "Michel De Montaigne": "\"I quote others only in order the better to express myself.\"",
+    "Winston Churchill": "\"Kites rise highest against the wind, not with it\"",
+    "Albert Einstein": "\"If we knew what we were doing, it would not be called research, would it?\""
+}    
+authors = Object.keys(quotes);
+count = 1;
+
+function newQuote() {
+    document.getElementById("quote").innerHTML = quotes[authors[count]];
+    document.getElementById("author").innerHTML = authors[count];
+//reset the counter, looping back to the first quote
+    count++;
+    if (count >= 4) {
+        count = 1;
+    }
+}
+
+
+// Event listener
